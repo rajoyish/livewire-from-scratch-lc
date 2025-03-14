d5c474789aa2182326fda5f253fe99ae18d745a2
Rajesh Budhathoki <rajoyish@gmail.com>
2025-03-14 08:51:36 +0545

    Working with radios and checkboxes

--- app/Livewire/Forms/ArticleForm.php
+++ app/Livewire/Forms/ArticleForm.php
@@ -18,14 +18,17 @@ class ArticleForm extends Form
 
     public $published = false;
 
-    public $notification = 'none';
+    public $notifications = [];
+
+    public $allowNotifications = false;
 
     public function setArticle(Article $article)
     {
         $this->title = $article->title;
         $this->content = $article->content;
         $this->published = $article->published;
-        $this->notification = $article->notification;
+        $this->notifications = $article->notifications ?? [];
+        $this->allowNotifications = count($this->notifications) > 0;
 
         $this->article = $article;
     }
@@ -34,15 +37,23 @@ class ArticleForm extends Form
     {
         $this->validate();
 
-        Article::create($this->only(['title', 'content', 'published', 'notification']));
+        if (! $this->allowNotifications) {
+            $this->notifications = [];
+        }
+
+        Article::create($this->only(['title', 'content', 'published', 'notifications']));
     }
 
     public function update()
     {
         $this->validate();
 
+        if (! $this->allowNotifications) {
+            $this->notifications = [];
+        }
+
         $this->article->update(
-            $this->only(['title', 'content', 'published', 'notification'])
+            $this->only(['title', 'content', 'published', 'notifications'])
         );
     }
 }
--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -43,63 +43,78 @@
                         </button>
 
                     </div>
-                    <div class="flex mt-6">
+
+                    <div class="flex my-6">
                         <input type="checkbox" wire:model.boolean="form.published"
-                            class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500 checked:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                            class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-green-600 focus:ring-green-500 checked:border-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
                             id="hs-default-checkbox">
-                        <label for="hs-default-checkbox"
-                            class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Published</label>
+                        <label for="hs-default-checkbox" class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Set as
+                            Published</label>
                     </div>
-                    <ul class="flex flex-col sm:flex-row mt-6">
-                        <li
-                            class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
-                            <div class="relative flex items-start w-full">
-                                <div class="flex items-center h-5">
-                                    <input wire:model="form.notification" value="email"
-                                        id="hs-horizontal-list-group-item-radio-1"
-                                        name="hs-horizontal-list-group-item-radio" type="radio"
-                                        class="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                                        checked="">
+                    <div class="space-y-4">
+                        <p class="font-bold">Need notifications?</p>
+                        <ul class="flex flex-col sm:flex-row mt-6">
+                            <li
+                                class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
+                                <div class="relative flex items-start w-full">
+                                    <div class="flex items-center h-5">
+                                        <input wire:model.boolean="form.allowNotifications" value="true"
+                                            id="hs-horizontal-list-group-item-radio-1"
+                                            name="hs-horizontal-list-group-item-radio" type="radio"
+                                            class="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
+                                            checked="">
+                                    </div>
+                                    <label for="hs-horizontal-list-group-item-radio-1"
+                                        class="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
+                                        Yes
+                                    </label>
                                 </div>
-                                <label for="hs-horizontal-list-group-item-radio-1"
-                                    class="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
-                                    Email
-                                </label>
-                            </div>
-                        </li>
+                            </li>
 
-                        <li
-                            class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
-                            <div class="relative flex items-start w-full">
-                                <div class="flex items-center h-5">
-                                    <input wire:model="form.notification" value="sms"
-                                        id="hs-horizontal-list-group-item-radio-2"
-                                        name="hs-horizontal-list-group-item-radio" type="radio"
-                                        class="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
+                            <li
+                                class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
+                                <div class="relative flex items-start w-full">
+                                    <div class="flex items-center h-5">
+                                        <input wire:model.boolean="form.allowNotifications" value="false"
+                                            id="hs-horizontal-list-group-item-radio-2"
+                                            name="hs-horizontal-list-group-item-radio" type="radio"
+                                            class="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800">
+                                    </div>
+                                    <label for="hs-horizontal-list-group-item-radio-2"
+                                        class="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
+                                        No
+                                    </label>
                                 </div>
-                                <label for="hs-horizontal-list-group-item-radio-2"
-                                    class="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
-                                    SMS
-                                </label>
-                            </div>
-                        </li>
+                            </li>
+                        </ul>
 
-                        <li
-                            class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
-                            <div class="relative flex items-start w-full">
-                                <div class="flex items-center h-5">
-                                    <input wire:model="form.notification" value="none"
-                                        id="hs-horizontal-list-group-item-radio-3"
-                                        name="hs-horizontal-list-group-item-radio" type="radio"
-                                        class="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
-                                </div>
-                                <label for="hs-horizontal-list-group-item-radio-3"
-                                    class="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
-                                    None
-                                </label>
+                        {{-- Notifications Checkboxes --}}
+                        <div x-show="$wire.form.allowNotifications" class="space-y-4">
+                            <div class="flex">
+                                <input type="checkbox" wire:model="form.notifications" value="email"
+                                    class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-green-600 focus:ring-green-500 checked:border-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
+                                    id="hs-default-checkbox">
+                                <label for="hs-default-checkbox"
+                                    class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Email</label>
                             </div>
-                        </li>
-                    </ul>
+
+                            <div class="flex">
+                                <input type="checkbox" wire:model="form.notifications" value="sms"
+                                    class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-green-600 focus:ring-green-500 checked:border-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
+                                    id="hs-checked-checkbox" checked="">
+                                <label for="hs-checked-checkbox"
+                                    class="text-sm text-gray-500 ms-3 dark:text-neutral-400">SMS</label>
+                            </div>
+
+                            <div class="flex">
+                                <input type="checkbox" wire:model="form.notifications" value="push"
+                                    class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-green-600 focus:ring-green-500 checked:border-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
+                                    id="hs-checked-checkbox" checked="">
+                                <label for="hs-checked-checkbox"
+                                    class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Push</label>
+                            </div>
+                        </div>
+                    </div>
                 </form>
                 <!-- End Form -->
             </div>
